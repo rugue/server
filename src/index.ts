34892961ebf16f7b64e3abb55e9767279441a130
Osarugue Enehizena <nehirugue@gmail.com>
2024-06-30 16:39:48 +0100

    added code for location

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 import express from "express";
 import { Request, Response } from "express";
-import * as os from "os";
+import axios from "axios";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get("/api/hello", (req: Request, res: Response) => {
+app.get("/api/hello", async (req: Request, res: Response) => {
   const visitorName = req.query.visitor_name || "Visitor";
   const clientIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  res.json({
-    client_ip: clientIp,
-    greeting: `Hello, ${visitorName}!`,
-  });
+
+  try {
+    const response = await axios.get(
+      `https://ipinfo.io/${clientIp}/json?token=your_ipinfo_token`
+    );
+    const location = response.data.city || "Unknown location";
+    res.json({
+      client_ip: clientIp,
+      location,
+      greeting: `Hello, ${visitorName}!`,
+    });
+  } catch (error) {
+    res.json({
+      client_ip: clientIp,
+      location: "Unable to determine location",
+      greeting: `Hello, ${visitorName}!`,
+    });
+  }
 });
 
 app.listen(port, () => {
