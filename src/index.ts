1bf9b556d7069363eb6692784fb11b5a3518034c
Osarugue Enehizena <nehirugue@gmail.com>
2024-06-30 18:23:17 +0100

    transformed ip

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,17 @@ const port = process.env.PORT || 8000;
 
 app.get("/api/hello", async (req: Request, res: Response) => {
   const visitorName = req.query.visitor_name || "Visitor";
-  const clientIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const clientIp =
+    req.headers["x-forwarded-for"] || req.socket.remoteAddress || "";
+  console.log({ clientIp });
+
+  let transformedClientIp = Array.isArray(clientIp) ? clientIp[0] : clientIp;
+
+  console.log({ transformedClientIp });
+
+  transformedClientIp = transformedClientIp?.split(",")[0] || "";
+
+  console.log({ transformedClientIp });
 
   try {
     const response = await axios.get(
@@ -17,7 +27,7 @@ app.get("/api/hello", async (req: Request, res: Response) => {
     const location = response.data.city || "Unknown location";
 
     res.json({
-      client_ip: clientIp,
+      client_ip: transformedClientIp,
       location,
       greeting: `Hello, ${visitorName}!`,
     });
